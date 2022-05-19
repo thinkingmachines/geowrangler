f7043336433885e64f6aac54945adad2bb10e3b2
Butch Landingin <butch@thinkingmachin.es>
2022-05-19 14:35:30 +0800

    Fix setup to handle python versions 3.9 and up

--- setup.py
+++ setup.py
@@ -44,9 +44,7 @@ statuses = [
     "6 - Mature",
     "7 - Inactive",
 ]
-py_versions = (
-    "2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 3.0 3.1 3.2 3.3 3.4 3.5 3.6 3.7 3.8".split()
-)
+py_versions = "2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 3.0 3.1 3.2 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10".split()
 
 requirements = cfg.get("requirements", "").split()
 min_python = cfg["min_python"]
