44205276c54cad12c6d1ef4610271d605f680115
Jt Miclat <jtmiclat@gmail.com>
2022-05-31 09:08:23 +0800

    Add Orientation and CRS Validation

--- geowrangler/_nbdev.py
+++ geowrangler/_nbdev.py
@@ -3,19 +3,28 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {
-    "logger": "00_grids.ipynb",
+    "logger": "00_validation.ipynb",
     "GridGenerator": "00_grids.ipynb",
-    "get_ranges": "00_grids.ipynb",
-    "create_grid": "00_grids.ipynb",
-    "generate_grids": "00_grids.ipynb",
     "GridGenerator.get_ranges": "00_grids.ipynb",
     "GridGenerator.create_grid": "00_grids.ipynb",
     "GridGenerator.generate_grids": "00_grids.ipynb",
+    "ValidationError": "00_validation.ipynb",
+    "Validator": "00_validation.ipynb",
+    "Validator.validate": "00_validation.ipynb",
+    "OrientationValidator": "00_validation.ipynb",
+    "OrientationValidator.check": "00_validation.ipynb",
+    "OrientationValidator.fix": "00_validation.ipynb",
+    "CrsBoundsValidator": "00_validation.ipynb",
+    "CrsBoundsValidator.check": "00_validation.ipynb",
+    "CrsBoundsValidator.fix": "00_validation.ipynb",
+    "CrsBoundsValidator.validate": "00_validation.ipynb",
+    "GeometryValidation": "00_validation.ipynb",
+    "GeometryValidation.validate_all": "00_validation.ipynb",
 }
 
-modules = ["grids.py"]
+modules = ["grids.py", "validation.py"]
 
-doc_url = "https://thinkingmachines.github.io/geowrangler/"
+doc_url = "https://thinkingmachines.github.io/"
 
 git_url = "https://github.com/thinkingmachines/geowrangler/tree/master/"
 
