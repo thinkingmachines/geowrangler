81a8a210917ee3c8a6154023cb2f05770c20727c
Butch Landingin <butch@thinkingmachin.es>
2022-06-20 17:01:51 +0800

    Add vector zonal stats impl

--- geowrangler/_nbdev.py
+++ geowrangler/_nbdev.py
@@ -26,9 +26,13 @@ index = {
     "NullValidator.fix": "00_validation.ipynb",
     "GeometryValidation": "00_validation.ipynb",
     "GeometryValidation.validate_all": "00_validation.ipynb",
+    "fix_agg": "02_vector_zonal_stats.ipynb",
+    "prep_aoi": "02_vector_zonal_stats.ipynb",
+    "aggregate_stats": "02_vector_zonal_stats.ipynb",
+    "create_zonal_stats": "02_vector_zonal_stats.ipynb",
 }
 
-modules = ["grids.py", "validation.py"]
+modules = ["grids.py", "validation.py", "vector_zonal_stats.py"]
 
 doc_url = "https://geowrangler.web.app/"
 
