617004d24730a1d8c7434aa9c78dbff96b5065fc
Butch Landingin <butch@thinkingmachin.es>
2022-07-21 15:23:52 +0800

    Add initial implementation of create_bingtile_zonal_stats

--- geowrangler/_nbdev.py
+++ geowrangler/_nbdev.py
@@ -37,9 +37,13 @@ index = {
     "GeometryValidation.validate_all": "00_validation.ipynb",
     "GEO_INDEX_NAME": "02_vector_zonal_stats.ipynb",
     "create_zonal_stats": "02_vector_zonal_stats.ipynb",
+    "tms": "02_vector_zonal_stats.ipynb",
     "get_quadkey": "02_vector_zonal_stats.ipynb",
     "compute_quadkey": "02_vector_zonal_stats.ipynb",
-    "tms": "02_vector_zonal_stats.ipynb",
+    "validate_aoi_quadkey": "02_vector_zonal_stats.ipynb",
+    "validate_data_quadkey": "02_vector_zonal_stats.ipynb",
+    "quadkey_to_tile": "02_vector_zonal_stats.ipynb",
+    "create_bingtile_zonal_stats": "02_vector_zonal_stats.ipynb",
 }
 
 modules = ["grids.py", "validation.py", "vector_zonal_stats.py"]
