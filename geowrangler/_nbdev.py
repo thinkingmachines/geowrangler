3700189bc1063e8f2bd05ad02af3741597a08950
Butch Landingin <butch@thinkingmachin.es>
2022-07-29 17:27:40 +0800

    Implement vector zonal stats over areas

--- geowrangler/_nbdev.py
+++ geowrangler/_nbdev.py
@@ -45,6 +45,8 @@ index = {
     "quadkey_to_tile": "02_vector_zonal_stats.ipynb",
     "create_bingtile_zonal_stats": "02_vector_zonal_stats.ipynb",
     "create_raster_zonal_stats": "03_raster_zonal_stats.ipynb",
+    "get_approximate_col_name": "04_dhs_data.ipynb",
+    "generate_dhs_cluster_data": "04_dhs_data.ipynb",
     "PH_COLUMN_CONFIG": "04_dhs.ipynb",
     "KH_COLUMN_CONFIG": "04_dhs.ipynb",
     "MM_COLUMN_CONFIG": "04_dhs.ipynb",
@@ -57,6 +59,19 @@ index = {
     "load_geofabrik_data": "05_datasets_geofabrik.ipynb",
     "list_geofabrik_regions": "05_datasets_geofabrik.ipynb",
     "download_geofabrik_region": "05_datasets_geofabrik.ipynb",
+    "prep_aoi": "04_area_zonal_stats.ipynb",
+    "extract_func": "04_area_zonal_stats.ipynb",
+    "fix_area_agg": "04_area_zonal_stats.ipynb",
+    "get_source_column": "04_area_zonal_stats.ipynb",
+    "INTERSECT_AREA_AGG": "04_area_zonal_stats.ipynb",
+    "build_agg_area_dicts": "04_area_zonal_stats.ipynb",
+    "validate_area_aoi": "04_area_zonal_stats.ipynb",
+    "validate_area_data": "04_area_zonal_stats.ipynb",
+    "validate_area_aggs": "04_area_zonal_stats.ipynb",
+    "expand_area_aggs": "04_area_zonal_stats.ipynb",
+    "compute_intersect_stats": "04_area_zonal_stats.ipynb",
+    "compute_imputed_stats": "04_area_zonal_stats.ipynb",
+    "create_area_zonal_stats": "04_area_zonal_stats.ipynb",
 }
 
 modules = [
@@ -66,6 +81,7 @@ modules = [
     "raster_zonal_stats.py",
     "dhs.py",
     "datasets/geofabrik.py",
+    "area_zonal_stats.py",
 ]
 
 doc_url = "https://geowrangler.web.app/"
