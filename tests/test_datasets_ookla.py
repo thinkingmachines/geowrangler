3b4c161e5100073afc54db309aad6d715646920b
Jt Miclat <jtmiclat@gmail.com>
2022-08-16 21:05:50 +0800

    Add ookla tests

---
+++ tests/test_datasets_ookla.py
@@ -0,0 +1,60 @@
+import shutil
+
+import pytest
+import requests
+
+from geowrangler.datasets import ookla
+
+
+@pytest.fixture
+def mock_ookla_req(monkeypatch):
+    class MockResponse:
+        def raw(*args, **kwargs):
+            return b""
+
+        @property
+        def text():
+            return """
+<?xml version="1.0" encoding="UTF-8"?>
+<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+<Name>ookla-open-data</Name>
+<Prefix>parquet</Prefix>
+<KeyCount>28</KeyCount>
+<MaxKeys>1000</MaxKeys>
+<IsTruncated>false</IsTruncated>
+<Contents>
+<Key>parquet/performance/type=fixed/year=2019/quarter=1/2019-01-01_performance_fixed_tiles.parquet</Key>
+<LastModified>2021-06-01T19:16:04.000Z</LastModified>
+<ETag>&quot;35a51f641831316ceffb0402f0e54550-50&quot;</ETag>
+<Size>414572196</Size>
+<StorageClass>INTELLIGENT_TIERING</StorageClass>
+</Contents>
+</ListBucketResult>
+            """
+
+    def mock_get(*args, **kwargs):
+        return MockResponse()
+
+    monkeypatch.setattr(requests, "get", mock_get)
+    yield
+
+
+def test_list_ookla_file(mock_geofabrike_req):
+    files = ookla.list_geofabrik_regions()
+    assert len(files.keys()) == 1
+    assert ookla.OoklaFile("fixed", "2019", "1") in files
+
+
+def test_download_geofabrik_region_no_dir(mock_geofabrike_req):
+    with pytest.raises(ValueError):
+        ookla.download_ookla_file("fixed", "2019", "1", "this-directory-does-not-exits")
+
+
+def test_download_geofabrik_region_no_region(mock_geofabrike_req, tmpdir):
+    with pytest.raises(ValueError):
+        ookla.download_ookla_file("fixed", "2020", "1", tmpdir)
+
+
+def test_download_geofabrik_regions(mock_geofabrike_req, monkeypatch, mocker, tmpdir):
+    monkeypatch.setattr(shutil, "copyfileobj", mocker.MagicMock())
+    ookla.download_ookla_file("fixed", "2019", "1", tmpdir)
