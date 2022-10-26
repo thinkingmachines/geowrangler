4a1eb7b9e31000c90452c45d2768d8b2c97e299a
Butch Landingin <butch@thinkingmachin.es>
2022-10-26 10:00:06 +0800

    Fix random number generation

--- tests/test_tile_clustering.py
+++ tests/test_tile_clustering.py
@@ -1,8 +1,6 @@
 import geopandas as gpd
 import numpy as np
-import pandas as pd
 import pytest
-from numpy.random import MT19937, RandomState, SeedSequence
 
 import geowrangler.grids as grids
 import geowrangler.tile_clustering as tc
@@ -10,8 +8,7 @@ import geowrangler.tile_clustering as tc
 
 @pytest.fixture()
 def grid_gdf5k():
-    rs = RandomState(MT19937(SeedSequence(123456789)))
-
+    rs = np.random.default_rng(seed=42)
     region3_gdf = gpd.read_file("data/region3_admin.geojson")
     grid_generator5k = grids.SquareGridGenerator(5_000)
     grid = grid_generator5k.generate_grid(region3_gdf)
@@ -24,14 +21,14 @@ def test_tile_clustering(grid_gdf5k):
     tileclustering = tc.TileClustering()
     gdf5k = tileclustering.cluster_tiles(grid_gdf5k, category_col="class")
     assert len(gdf5k) == 1074
-    assert gdf5k["tile_cluster"].nunique() == 167
+    assert gdf5k["tile_cluster"].nunique() == 159
 
 
 def test_tile_clustering_eightway(grid_gdf5k):
     tileclustering = tc.TileClustering(cluster_type="eight_way")
     gdf5k = tileclustering.cluster_tiles(grid_gdf5k, category_col="class")
     assert len(gdf5k) == 1074
-    assert gdf5k["tile_cluster"].nunique() == 69
+    assert gdf5k["tile_cluster"].nunique() == 70
 
 
 def test_tile_clustering_no_category(grid_gdf5k):
